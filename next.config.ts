6d82897c6b78c11718e05c445c78e4e94d9b9af5
SayanDasDev <mesayan19@gmail.com>
2024-12-13 17:20:53 +0530

    template setup

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  /* for static site generation */
+  // reactStrictMode: true,
+  // output: 'export',
+  // images: {
+  //   unoptimized: true, // Image optimization is disabled in static builds
+  // },
+  // distDir: 'build',
+
+  experimental: {
+    typedRoutes: true,
+  }
 };
 
 export default nextConfig;
