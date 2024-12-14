202bd7a537703bce19647a50a7e73f8c2cb3c41a
SayanDasDev <mesayan19@gmail.com>
2024-12-14 16:06:57 +0530

    sponsors fixed

--- src/components/sponsors/sponsors.tsx
+++ src/components/sponsors/sponsors.tsx
@@ -10,10 +10,30 @@ const Sponsors = () => {
     <div className="w-full flex flex-col items-center justify-center space-y-12">
       <Heading heading="Sponsors" />
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 px-4 gap-10 max-w-7xl mx-auto w-full">
-        <GlareCard className="flex flex-col items-center justify-center text-background ">
-          <GitGraph size={64} />
-          <p className="text-white font-bold text-xl mt-4">Aceternity</p>
-        </GlareCard>
+        <div className="flex justify-center items-center">
+          <GlareCard className="flex flex-col items-center justify-center text-background !mx-auto">
+            <GitGraph size={64} />
+            <p className="text-white font-bold text-xl mt-4">Aceternity</p>
+          </GlareCard>
+        </div>
+        <div className="flex justify-center items-center">
+          <GlareCard className="flex flex-col items-center justify-center text-background !mx-auto">
+            <GitGraph size={64} />
+            <p className="text-white font-bold text-xl mt-4">Aceternity</p>
+          </GlareCard>
+        </div>
+        <div className="flex justify-center items-center">
+          <GlareCard className="flex flex-col items-center justify-center text-background !mx-auto">
+            <GitGraph size={64} />
+            <p className="text-white font-bold text-xl mt-4">Aceternity</p>
+          </GlareCard>
+        </div>
+        <div className="flex justify-center items-center">
+          <GlareCard className="flex flex-col items-center justify-center text-background !mx-auto">
+            <GitGraph size={64} />
+            <p className="text-white font-bold text-xl mt-4">Aceternity</p>
+          </GlareCard>
+        </div>
       </div>
     </div>
   );
