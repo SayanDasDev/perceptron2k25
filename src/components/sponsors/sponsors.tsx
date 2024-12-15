338ff9e96cc52e1b19b9b2b4a4dea88321f1821d
SayanDasDev <mesayan19@gmail.com>
2024-12-15 13:38:48 +0530

    sponsors fixed

--- src/components/sponsors/sponsors.tsx
+++ src/components/sponsors/sponsors.tsx
@@ -1,17 +1,13 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
-import React from "react";
 
 import { Facebook, Globe, Instagram, Linkedin, Youtube } from "lucide-react";
 
 import { sponsorData } from "@/config/sponsors";
-import { cn } from "@/lib/utils";
 
 import Heading from "../heading";
-import { buttonVariants } from "../ui/button";
-import { GlareCard } from "../ui/glare-card";
+import { CardBody, CardContainer, CardItem } from "../ui/3d-card";
 
 const Sponsors = () => {
   return (
@@ -19,86 +15,37 @@ const Sponsors = () => {
       <Heading heading="Sponsors" />
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 px-4 gap-10 max-w-7xl mx-auto w-full">
         {sponsorData.map((sponsor, index) => (
-          <div key={index} className="flex justify-center items-center">
-            <GlareCard className="flex flex-col items-center justify-center text-background !mx-auto px-4">
-              <Image
-                src={sponsor.imageURL}
-                width={200}
-                height={200}
-                alt={sponsor.name}
-                className="h-40 object-contain w-full"
-              />
-              {/* <p className="text-background font-bold text-xl mt-4">
-                {sponsor.name}
-              </p> */}
-              <p className="text-background/75 font-semibold text-lg mt-8">
-                {sponsor.title}
-              </p>
-              <div className="grid grid-cols-3 flex-wrap gap-2 mt-4 !z-[10000]">
+          <CardContainer key={index} className="flex flex-1 inter-var">
+            <CardBody className="bg-slate-950 hover:bg-slate-900 flex flex-col items-center relative group/card hover:shadow-2xl hover:shadow-emerald-500/[0.1] border-white/[0.2] border-black/[0.1] w-full max-w-[380px] h-auto rounded-xl p-6 border  ">
+              <CardItem translateZ="100" className="w-full mt-4">
+                <Image
+                  src={sponsor.imageURL}
+                  width={200}
+                  height={200}
+                  alt={sponsor.name}
+                  className="h-40 w-full object-contain rounded-xl group-hover/card:shadow-xl"
+                />
+              </CardItem>
+              <div className="grid grid-cols-3 flex-wrap gap-2 mt-10">
                 {Object.entries(sponsor.links || {}).map(
                   ([social, link], index) => (
-                    <React.Fragment key={index}>
-                      {social == "website" && (
-                        <Link
-                          className={cn(
-                            buttonVariants({ variant: "ghost" }),
-                            "bg-background rounded-full mr-4 text-foreground z-[10000]"
-                          )}
-                          href={link}
-                        >
-                          <Globe size={16} />
-                        </Link>
-                      )}
-                      {social == "linkedin" && (
-                        <Link
-                          className={cn(
-                            buttonVariants({ variant: "ghost" }),
-                            "bg-background rounded-full mr-4 text-foreground z-[10000]"
-                          )}
-                          href={link}
-                        >
-                          <Linkedin size={16} />
-                        </Link>
-                      )}
-                      {social == "facebook" && (
-                        <Link
-                          className={cn(
-                            buttonVariants({ variant: "ghost" }),
-                            "bg-background rounded-full mr-4 text-foreground z-[10000]"
-                          )}
-                          href={link}
-                        >
-                          <Facebook size={16} />
-                        </Link>
-                      )}
-                      {social == "instagram" && (
-                        <Link
-                          className={cn(
-                            buttonVariants({ variant: "ghost" }),
-                            "bg-background rounded-full mr-4 text-foreground z-[10000]"
-                          )}
-                          href={link}
-                        >
-                          <Instagram size={16} />
-                        </Link>
-                      )}
-                      {social == "youtube" && (
-                        <Link
-                          className={cn(
-                            buttonVariants({ variant: "ghost" }),
-                            "bg-background rounded-full mr-4 text-foreground z-[10000]"
-                          )}
-                          href={link}
-                        >
-                          <Youtube size={16} />
-                        </Link>
-                      )}
-                    </React.Fragment>
+                    <CardItem
+                      translateZ={20}
+                      as="button"
+                      onClick={() => window.open(link, "_blank")}
+                      className="px-4 py-2 bg-background rounded-full text-foreground/80 hover:text-foreground"
+                    >
+                      {social == "website" && <Globe size={16} />}
+                      {social == "linkedin" && <Linkedin size={16} />}
+                      {social == "facebook" && <Facebook size={16} />}
+                      {social == "instagram" && <Instagram size={16} />}
+                      {social == "youtube" && <Youtube size={16} />}
+                    </CardItem>
                   )
                 )}
               </div>
-            </GlareCard>
-          </div>
+            </CardBody>
+          </CardContainer>
         ))}
       </div>
     </div>
