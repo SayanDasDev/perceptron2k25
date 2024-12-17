e7351bf9ead1b9b056ee1b2353b68b0544d5ec2a
SayanDasDev <mesayan19@gmail.com>
2024-12-17 16:31:31 +0530

    brochure updated

--- src/components/about/about-video.tsx
+++ src/components/about/about-video.tsx
@@ -8,7 +8,7 @@ export function AboutVideo() {
         animationStyle="top-in-bottom-out"
         videoSrc="https://www.youtube.com/embed/AQ0y952kZyA?si=IjfPc3v05bwdTkN"
         thumbnailSrc="https://raw.githubusercontent.com/SayanDasDev/perceptron2k25/refs/heads/main/public/images/youtube-thumbnail.png"
-        thumbnailAlt="Hero Video"
+        thumbnailAlt="About Video"
       />
     </div>
   );
