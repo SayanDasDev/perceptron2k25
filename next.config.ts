99d8854598cc4f630bb00e37b4dc63e004d06253
SayanDasDev <mesayan19@gmail.com>
2024-12-17 13:19:09 +0530

    bsepath added

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
   },
   distDir: 'build',
 
-  // basePath: '/home/debanjan/perceptron/perceptron2k25/build',
+  basePath: '/home/debanjan/perceptron/perceptron2k25/build',
   assetPrefix: '/home/debanjan/perceptron/perceptron2k25/build/',
 
 
